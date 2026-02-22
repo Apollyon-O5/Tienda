43610248c192a382c2e4a9a5ef5835d897eadaa3
Roberto <af3595012024@unab.edu.sv>
2026-02-22 12:55:20 -0600

    Botones creados y conexion con la api

--- Tienda/Form1.Designer.cs
+++ Tienda/Form1.Designer.cs
@@ -28,12 +28,70 @@
         /// </summary>
         private void InitializeComponent()
         {
-            components = new System.ComponentModel.Container();
+            flowPokemon = new FlowLayoutPanel();
+            btnAnterior = new Button();
+            btnSiguiente = new Button();
+            lblSaldo = new Label();
+            flowPokemon.SuspendLayout();
+            SuspendLayout();
+            // 
+            // flowPokemon
+            // 
+            flowPokemon.AutoScroll = true;
+            flowPokemon.Controls.Add(btnAnterior);
+            flowPokemon.Controls.Add(btnSiguiente);
+            flowPokemon.Controls.Add(lblSaldo);
+            flowPokemon.Dock = DockStyle.Fill;
+            flowPokemon.Location = new Point(0, 0);
+            flowPokemon.Name = "flowPokemon";
+            flowPokemon.Size = new Size(800, 450);
+            flowPokemon.TabIndex = 0;
+            // 
+            // btnAnterior
+            // 
+            btnAnterior.Location = new Point(3, 3);
+            btnAnterior.Name = "btnAnterior";
+            btnAnterior.Size = new Size(79, 24);
+            btnAnterior.TabIndex = 0;
+            btnAnterior.Text = "Anterior";
+            btnAnterior.UseVisualStyleBackColor = true;
+            // 
+            // btnSiguiente
+            // 
+            btnSiguiente.Location = new Point(88, 3);
+            btnSiguiente.Name = "btnSiguiente";
+            btnSiguiente.Size = new Size(79, 24);
+            btnSiguiente.TabIndex = 1;
+            btnSiguiente.Text = "Siguiente";
+            btnSiguiente.UseVisualStyleBackColor = true;
+            // 
+            // lblSaldo
+            // 
+            lblSaldo.AutoSize = true;
+            lblSaldo.Location = new Point(173, 0);
+            lblSaldo.Name = "lblSaldo";
+            lblSaldo.Size = new Size(0, 17);
+            lblSaldo.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 17F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Controls.Add(flowPokemon);
+            Name = "Form1";
             Text = "Form1";
+            Load += Form1_Load;
+            flowPokemon.ResumeLayout(false);
+            flowPokemon.PerformLayout();
+            ResumeLayout(false);
         }
 
         #endregion
+
+        private FlowLayoutPanel flowPokemon;
+        private Button btnAnterior;
+        private Button btnSiguiente;
+        private Label lblSaldo;
     }
 }
--- Tienda/Form1.cs
+++ Tienda/Form1.cs
@@ -1,10 +1,146 @@
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text.Json;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
 namespace Tienda
 {
     public partial class Form1 : Form
     {
+        private HttpClient client = new HttpClient();
+        private int paginaActual = 1;
+        private int porPagina = 12;
+        private int saldo = 200;
+
         public Form1()
         {
             InitializeComponent();
         }
+
+        private async void Form1_Load(object sender, EventArgs e)
+        {
+            lblSaldo.Text = "Saldo: " + saldo + " Pokébolas";
+            await CargarPagina();
+        }
+
+        private async Task CargarPagina()
+        {
+            flowPokemon.Controls.Clear();
+
+            int inicio = (paginaActual - 1) * porPagina + 1;
+            int fin = inicio + porPagina;
+
+            for (int i = inicio; i < fin; i++)
+            {
+                await CrearTarjetaPokemon(i);
+            }
+        }
+
+        private async Task CrearTarjetaPokemon(int id)
+        {
+            var response = await client.GetAsync($"https://pokeapi.co/api/v2/pokemon/{id}");
+            var json = await response.Content.ReadAsStringAsync();
+            var pokemon = JsonSerializer.Deserialize<Pokemon>(json);
+
+            Panel card = new Panel();
+            card.Width = 150;
+            card.Height = 220;
+            card.BorderStyle = BorderStyle.FixedSingle;
+
+            PictureBox pic = new PictureBox();
+            pic.Width = 120;
+            pic.Height = 100;
+            pic.Top = 10;
+            pic.Left = 15;
+            pic.SizeMode = PictureBoxSizeMode.Zoom;
+            pic.Load(pokemon.sprites.front_default);
+
+            Label lblNombre = new Label();
+            lblNombre.Text = pokemon.name.ToUpper();
+            lblNombre.Top = 115;
+            lblNombre.Left = 10;
+            lblNombre.Width = 130;
+
+            Label lblTipo = new Label();
+            lblTipo.Text = "Tipo: " + pokemon.types[0].type.name;
+            lblTipo.Top = 135;
+            lblTipo.Left = 10;
+            lblTipo.Width = 130;
+
+            int precio = id * 2;
+
+            Label lblPrecio = new Label();
+            lblPrecio.Text = "Precio: " + precio;
+            lblPrecio.Top = 155;
+            lblPrecio.Left = 10;
+            lblPrecio.Width = 130;
+
+            Button btnComprar = new Button();
+            btnComprar.Text = "Comprar";
+            btnComprar.Top = 175;
+            btnComprar.Left = 25;
+            btnComprar.Width = 100;
+
+            btnComprar.Click += (s, e) =>
+            {
+                if (saldo >= precio)
+                {
+                    saldo -= precio;
+                    lblSaldo.Text = "Saldo: " + saldo + " Pokébolas";
+                    MessageBox.Show($"Compraste a {pokemon.name}");
+                }
+                else
+                {
+                    MessageBox.Show("No tienes suficientes Pokébolas");
+                }
+            };
+
+            card.Controls.Add(pic);
+            card.Controls.Add(lblNombre);
+            card.Controls.Add(lblTipo);
+            card.Controls.Add(lblPrecio);
+            card.Controls.Add(btnComprar);
+
+            flowPokemon.Controls.Add(card);
+        }
+
+        private async void btnSiguiente_Click(object sender, EventArgs e)
+        {
+            paginaActual++;
+            await CargarPagina();
+        }
+
+        private async void btnAnterior_Click(object sender, EventArgs e)
+        {
+            if (paginaActual > 1)
+            {
+                paginaActual--;
+                await CargarPagina();
+            }
+        }
+    }
+
+    public class Pokemon
+    {
+        public string name { get; set; }
+        public List<TypeInfo> types { get; set; }
+        public Sprites sprites { get; set; }
+    }
+
+    public class TypeInfo
+    {
+        public TypeDetail type { get; set; }
+    }
+
+    public class TypeDetail
+    {
+        public string name { get; set; }
+    }
+
+    public class Sprites
+    {
+        public string front_default { get; set; }
     }
 }
